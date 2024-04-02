977055d73b22f8c97cda6ca65821dac4e13ad878
Marcel Roesink <mroesink@rocvantwente.nl>
2024-04-02 13:21:28 +0200

    Add project files.

---
+++ ToetsCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Voeg hieronder een lijst met vakken toe
+
+        // Voeg hieronder een lege lijst met resultaten toe
+
+        // Laat de gebruiker al zijn eigen gegevens invoeren, zodat hij/zij
+        // zichzelf als student kan registreren
+
+        // Laat de gebruiker net zo lang resultaten toevoegen totdat hij/zij
+        // aangeeft te willen stoppen
+
+        // Geef een overzicht van alle resultaten van de student
+
+        Console.WriteLine();
+        Console.WriteLine("Druk op een willekeurige toets om af te sluiten");
+        Console.ReadKey();
+    }
+}
